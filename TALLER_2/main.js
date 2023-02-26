0f9a69b1c1dbdbce8c8f9c562d95d5121b4f8ab7
Yursksf1 <Yurs.Ksf1@gmail.com>
2023-02-25 21:41:13 -0500

    update proposal

--- TALLER_2/main.js
+++ TALLER_2/main.js
@@ -3,20 +3,48 @@ const plato_p=document.getElementById("plato_principal")
 const postres=document.getElementById("postres")
 const bebidas=document.getElementById("bebidas")
 const productos=document.getElementById("plato_principal_product")
-const principal_product=document.getElementsByClassName("plato_principal_product")
-const bebidas_product=document.getElementsByClassName("bebidas_product")
-const postres_product=document.getElementsByClassName("postres_product")
+const principal_product=document.querySelectorAll(".plato_principal_product")
+const bebidas_product=document.querySelectorAll(".bebidas_product")
+const postres_product=document.querySelectorAll(".postres_product")
 
-console.log(principal_product)
 todos.addEventListener("click",todos_platos);
 function todos_platos(){
-   principal_product.style.display="flex";
-   bebidas_product.style.display="flex";
-   postres_product.style.display="flex";
+    principal_product.forEach(element => {
+        element.style.display="flex";
+    });
+
+    bebidas_product.forEach(element => {
+        element.style.display="flex";
+    });
+
+    postres_product.forEach(element => {
+        element.style.display="flex";
+    });
 } 
-plato_p.addEventListener("click",principal);
-function principal(){
-    principal_product.style.display="none";
-    bebidas_product.style.display="none"
-    postres_product.style.display="none"
+
+plato_p.addEventListener("click", showPrincipal);
+
+function showPrincipal(){
+    principal_product.forEach(element => {
+        element.style.display="flex";
+    });
+    bebidas_product.forEach(element => {
+        element.style.display="none";
+    });
+    postres_product.forEach(element => {
+        element.style.display="none";
+    });
+} 
+
+bebidas.addEventListener("click", showBebidas);
+function showBebidas(){
+    principal_product.forEach(element => {
+        element.style.display="none";
+    });
+    bebidas_product.forEach(element => {
+        element.style.display="flex";
+    });
+    postres_product.forEach(element => {
+        element.style.display="none";
+    });
 } 
